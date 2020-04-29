1cbcb22f8a94f770daae8ac0dbd3de71babc2548
picolo190 <strilciuc_gabriel98@yahoo.ro>
2020-04-29 11:24:09 +0300

    primul task lab 10

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const bodyParser = require('body-parser')
-
 const app = express();
-
 const port = 6789;
 
 // directorul 'views' va conține fișierele .ejs (html + js executat la server)
 app.set('view engine', 'ejs');
+
 // suport pentru layout-uri - implicit fișierul care reprezintă template-ul site-ului este views/layout.ejs
 app.use(expressLayouts);
+
 // directorul 'public' va conține toate resursele accesibile direct de către client (e.g., fișiere css, javascript, imagini)
-app.use(express.static('public'))
-    // corpul mesajului poate fi interpretat ca json; datele de la formular se găsesc în format json în req.body
+app.use(express.static('public'));
+
+// corpul mesajului poate fi interpretat ca json; datele de la formular se găsesc în format json în req.body
 app.use(bodyParser.json());
+
 // utilizarea unui algoritm de deep parsing care suportă obiecte în obiecte
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -25,11 +27,15 @@ app.get('/', (req, res) => res.send('Hello World'));
 // la accesarea din browser adresei http://localhost:6789/chestionar se va apela funcția specificată
 app.get('/chestionar', (req, res) => {
     const listaIntrebari = [{
-            intrebare: 'Întrebarea 1',
-            variante: ['varianta 1', 'varianta 2', 'varianta 3', 'varianta 4'],
+            intrebare: 'Sunteti pasionat de carti?',
+            variante: ['Da', 'Nu', 'Citesc ocazional'],
             corect: 0
         },
-        //...
+        {
+            intrebare: 'Ati achizitionat carti de pe site-ul nostru?',
+            variante: ['Da', 'Nu'],
+            corect: 0
+        }
     ];
     // în fișierul views/chestionar.ejs este accesibilă variabila 'intrebari' care conține vectorul de întrebări
     res.render('chestionar', { intrebari: listaIntrebari });
